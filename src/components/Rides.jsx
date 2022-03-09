47905627caf362b4563dc73b86c472e10d3108d9
Swapnil074 <mailswapnil0074@gmail.com>
2022-03-09 09:49:54 +0530

    Tabs

--- src/components/Rides.jsx
+++ src/components/Rides.jsx
@@ -1,25 +1,41 @@
 import React from "react";
-import {
-  Tab,
-  Tabs,
-  TabList,
-  TabPanels,
-  TabPanel,
-  flexbox,
-  Box,
-} from "@chakra-ui/react";
+import { Tab, Tabs, TabList, TabPanels, TabPanel, Box } from "@chakra-ui/react";
 
 export default function Rides() {
   return (
-    <Box bg="#101010">
-      <Tabs>
-        <TabList>
-          <Tab>One</Tab>
-          <Tab>Two</Tab>
-          <Tab>Three</Tab>
+    <Box pl={5} justify="space-between">
+      <Tabs variant="unstyled" size="lg">
+        <TabList color="gray">
+          <Tab
+            _selected={{
+              color: "white",
+              borderBottom: "2px solid",
+              borderBottomColor: "white",
+            }}
+          >
+            Nearest rides
+          </Tab>
+          <Tab
+            _selected={{
+              color: "white",
+              borderBottom: "2px solid",
+              borderBottomColor: "white",
+            }}
+          >
+            Upcoming rides
+          </Tab>
+          <Tab
+            _selected={{
+              color: "white",
+              borderBottom: "2px solid",
+              borderBottomColor: "white",
+            }}
+          >
+            Past rides
+          </Tab>
         </TabList>
 
-        <TabPanels>
+        <TabPanels color="white">
           <TabPanel>
             <p>one!</p>
           </TabPanel>
